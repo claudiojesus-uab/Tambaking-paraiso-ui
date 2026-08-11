4326cdd78f083734ff4a9409b34073e01fdebf7c
Claudio-04 <rodrigo.rbj59@gmail.com>
2026-08-10 21:46:09 -0400

    aplicando php

---
+++ procesar.php
@@ -0,0 +1,9 @@
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+echo "Nombre: " . $nombre . "<br>";
+echo "Email: " . $email . "<br>";
+echo "Mensaje: " . $mensaje . "<br>";
+?>
